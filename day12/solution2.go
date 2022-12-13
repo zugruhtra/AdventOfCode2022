94af272c070be2a03cccb4e95247831794027b2a
Arthur Guz <zugruhtra@gmail.com>
2022-12-13 20:42:37 +0100

    Added second solution for day 12

---
+++ day12/solution2.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+)
+
+const INF = math.MaxInt32
+
+type Position struct {
+	x, y int
+}
+
+type Graph struct {
+	edges    []*Edge
+	vertices []int
+}
+
+type Edge struct {
+	From, To, Weight int
+}
+
+func NewEdge(from, to, weight int) *Edge {
+	return &Edge{from, to, weight}
+}
+
+func NewGraph(edges []*Edge, vertices []int) *Graph {
+	return &Graph{edges, vertices}
+}
+
+func (g *Graph) BellmanFord(source int) ([]int, []int) {
+	size := len(g.vertices)
+	distances := make([]int, size)
+	predecessors := make([]int, size)
+
+	for _, v := range g.vertices {
+		distances[v] = INF
+	}
+	distances[source] = 0
+
+	for i, changes := 0, 0; i < size-1; i, changes = i+1, 0 {
+		for _, edge := range g.edges {
+			newDist := distances[edge.From] + edge.Weight
+			if newDist < distances[edge.To] && edge.Weight < 2 {
+				distances[edge.To] = newDist
+				predecessors[edge.To] = edge.From
+				changes++
+			}
+		}
+		if changes == 0 {
+			break
+		}
+	}
+
+	return predecessors, distances
+}
+
+func EachLineDo(f func(string)) error {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		f(line)
+	}
+
+	err := scanner.Err()
+
+	return err
+}
+
+func main() {
+	var heightmap [][]int
+	var end Position
+
+	heightmap = make([][]int, 0)
+	row := 0
+
+	err := EachLineDo(func(line string) {
+		heightmap = append(heightmap, make([]int, 0))
+		for i, c := range line {
+			switch c {
+			case 'S':
+				heightmap[row] = append(heightmap[row], 0)
+			case 'E':
+				end = Position{i, row}
+				heightmap[row] = append(heightmap[row], 25)
+			default:
+				heightmap[row] = append(heightmap[row], int(c-'a'))
+			}
+		}
+		row++
+	})
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "reading stdin:", err)
+		os.Exit(1)
+	}
+
+	nRows := len(heightmap)
+	nCols := len(heightmap[0])
+	nVertices := nRows * nCols
+
+	vertices := make([]int, nVertices)
+	for i := 0; i < nVertices; i++ {
+		vertices[i] = i
+	}
+
+	edges := make([]*Edge, 0)
+	for i := 0; i < nRows; i++ {
+		for j := 0; j < nCols; j++ {
+			p := j + i*nCols
+			// left
+			if j-1 >= 0 {
+				edge := NewEdge(p, p-1, heightmap[i][j-1]-heightmap[i][j])
+				edges = append(edges, edge)
+			}
+			// right
+			if j+1 < nCols {
+				edge := NewEdge(p, p+1, heightmap[i][j+1]-heightmap[i][j])
+				edges = append(edges, edge)
+			}
+			// top
+			if p-nCols >= 0 {
+				edge := NewEdge(p, p-nCols, heightmap[i-1][j]-heightmap[i][j])
+				edges = append(edges, edge)
+			}
+			// down
+			if p+nCols < nVertices {
+				edge := NewEdge(p, p+nCols, heightmap[i+1][j]-heightmap[i][j])
+				edges = append(edges, edge)
+			}
+		}
+	}
+
+	possibleSource := make([]int, 0)
+
+	for i := 0; i < nRows; i++ {
+		for j := 0; j < nCols; j++ {
+			if heightmap[i][j] == 0 {
+				possibleSource = append(possibleSource, j+i*nCols)
+			}
+		}
+	}
+
+	goal := end.x + end.y*nCols
+	graph := NewGraph(edges, vertices)
+	allSteps := make([]int, 0)
+
+	for _, source := range possibleSource {
+		pred, _ := graph.BellmanFord(source)
+		steps := 0
+		prev := goal
+		seen := make(map[int]bool)
+		cycle := false
+
+		for prev != source {
+			seen[prev] = true
+			prev = pred[prev]
+			steps++
+			if _, prs := seen[prev]; prs {
+				cycle = true
+				break
+			}
+		}
+
+		if !cycle {
+			allSteps = append(allSteps, steps)
+		}
+	}
+
+	sort.Ints(allSteps)
+	fmt.Println("Num. of fewest Steps:", allSteps[0])
+}
